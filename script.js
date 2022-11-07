7268ee9dcd742241ed49344504745e850e848547
Serzat Demirer <serzat.demirer1@gmail.com>
2022-11-07 21:29:43 +0100

    html and css files are uppdated, Time and Date constructed with setInterval

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-const dateElement = document.getElementById("#current-date");
-const timeElement = document.getElementById("#current-time");
-const amPmElement = document.getElementById("#am-pm");
+const dateElement = document.getElementById("current-date");
+let timeElement = document.getElementById("current-time");
 
-const tempCelsius = document.getElementById("#temp-celsius");
-const tempFahrenheit = document.getElementById("#temp-fahrenheit");
+const tempCelsius = document.getElementById("temp-celsius");
+const tempFahrenheit = document.getElementById("temp-fahrenheit");
 
-const cityElement = document.getElementById("#city");
+const cityElement = document.getElementById("city");
+
+const days = [
+  "Söndag",
+  "Måndag",
+  "Tisdag",
+  "Onsdag",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "Januari",
+  "Februari",
+  "Mars",
+  "April",
+  "Maj",
+  "Juni",
+  "Juli",
+  "Augusti",
+  "September",
+  "Oktober",
+  "November",
+  "December",
+];
 
 setInterval(() => {
   const time = new Date();
   const month = time.getMonth();
   const date = time.getDate();
   const day = time.getDay();
-});
+  let hours = time.getHours();
+  const minutes = time.getMinutes();
+
+  timeElement.innerHTML = hours + ":" + minutes;
+  dateElement.innerHTML = days[day] + "," + " " + date + " " + months[month];
+}, 1000);
